18b3933ee1304e957328d34b09c089066d919c3a
Christian Castro <alvescastro00cca13@gmail.com>
2020-01-04 00:15:43 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+
+var vel = 34.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel>60){
+    console.log(`Você ultrapassou a velocidade permitida`)
+}
+else{
+    console.log("Você esttá dentro da lei")
+}
+console.log("Dirija com segurança")

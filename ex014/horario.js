b6c702f689eff91497e4157cc89c5ae469dad67a
Christian Castro <alvescastro00cca13@gmail.com>
2020-01-07 00:04:02 -0300

    Mostrador de horário

---
+++ ex014/horario.js
@@ -0,0 +1,19 @@
+function carregar(){
+    var msg = document.querySelector("#msg")
+    var img = document.querySelector("img")
+    var data = new Date()
+    var hora = data.getHours()
+
+    hora = 11
+    
+    msg.innerHTML = `São exatamente ${hora} horas`
+
+    if (hora >= 00 && hora <=11) {
+        img.src ="manha.jpeg"
+        
+    }else if(hora >=12 && hora <=17){
+        img.src="tarde.jpg"
+    }else{
+        img.src="noite.jpg"
+    }
+}

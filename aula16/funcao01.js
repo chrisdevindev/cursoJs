7510d22725fa67ce3d5dc3401f28786770c11c3f
Christian Castro <alvescastro00cca13@gmail.com>
2020-01-15 01:29:31 -0300

    aula16-funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,14 @@
+function parimpar(n){
+    if(n%2 == 0){
+        return 'Par!'
+    }
+    else{
+        return 'Impar!'
+    }
+}
+console.log(parimpar(23434))
+
+/////////////////////////////////////
+
+var r = parimpar(3)
+console.log(r)
---
+++ aula16/funcao04.js
@@ -0,0 +1,9 @@
+function fatorial(numero) {
+    if (numero == 1) {
+        return 1
+    }else{
+        return numero* fatorial(numero-1)
+    }
+    
+}
+console.log(fatorial(5))
